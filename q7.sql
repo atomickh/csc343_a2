13a642fcc6d6c64013c14e66bac719d96ea8f2e9
Kevin Hua <Kevin Hua>
2017-03-16 12:32:08 -0400

    asdf

--- q7.sql
+++ q7.sql
@@ -10,7 +10,13 @@ CREATE TABLE q7 (
 
 -- You may find it convenient to do this for each of the views
 -- that define your intermediate steps.  (But give them better names!)
-DROP VIEW IF EXISTS all_ts_pairs, all_tas_pairs, all_pairs, exist_pairs, exist_tas_pairs, exist_ts_pairs CASCADE;
+DROP VIEW IF EXISTS all_ts_pairs CASCADE;
+DROP VIEW IF EXISTS all_tas_pairs CASCADE;
+DROP VIEW IF EXISTS all_pairs CASCADE;
+DROP VIEW IF EXISTS exist_pairs CASCADE;
+DROP VIEW IF EXISTS exist_tas_pairs CASCADE;
+DROP VIEW IF EXISTS exist_ts_pairs CASCADE;
+DROP VIEW IF EXISTS not_output_ta CASCADE;
 
 -- Define views for your intermediate steps here.
 -- Every ta, group, assignment combination and the ones that actually occured
@@ -24,9 +30,7 @@ CREATE VIEW all_pairs AS (
 
 CREATE VIEW exist_pairs AS (
 	SELECT t1.username, t1.group_id, t2.assignment_id
-	FROM Grader AS t1
-		JOIN AssignmentGroup As t2
-		ON (t1.group_id = t2.group_id)
+	FROM Grader AS t1 NATURAL JOIN AssignmentGroup As t2
 );
 
 -- All student TA Pairs that can occur and actually occurred
@@ -59,14 +63,20 @@ CREATE VIEW exist_tas_pairs AS(
 	FROM exist_pairs
 );
 
-
-
--- Final answer.
-INSERT INTO q7 (
+CREATE VIEW not_output_ta AS (
 	SELECT username AS ta
 	FROM ((SELECT DISTINCT username 
 	      FROM (SELECT * FROM all_ts_pairs EXCEPT SELECT * FROM exist_ts_pairs) AS t1) 
 	INTERSECT ( SELECT DISTINCT username
 	      FROM (SELECT * FROM all_tas_pairs EXCEPT SELECT * FROM exist_tas_pairs) AS t2)) AS t3
+);
+
+
+-- Final answer.
+INSERT INTO q7 (
+	SELECT username AS ta 
+	FROM MarkusUser WHERE type = 'TA' 
+	EXCEPT
+	SELECT * FROM not_output_ta
 );
 	-- put a final query here so that its results will go into the table.
