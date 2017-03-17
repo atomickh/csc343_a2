3d42fc5ba4efecadf4d6196b97f420d0754fcf49
Kevin Hua <Kevin Hua>
2017-03-17 11:43:02 -0400

    minor fixes

--- q7.sql
+++ q7.sql
@@ -29,13 +29,12 @@ CREATE VIEW all_pairs AS (
 		ON (t1.type = 'TA')
 );	
 
-
 CREATE VIEW exist_pairs AS (
 	SELECT t1.username, t1.group_id, t2.assignment_id
 	FROM Grader AS t1 NATURAL JOIN AssignmentGroup As t2
 );
 
--- All student TA Pairs that can occur and actually occurred
+-- All student & TA Pairs that can occur and actually occurred
 CREATE VIEW all_ts_pairs AS(
 	SELECT t1.username, t2.username AS student
 	FROM MarkusUser AS t1 
@@ -54,7 +53,7 @@ CREATE VIEW exist_ts_pairs AS(
 		ON (t1.group_id = t2.group_id)
 );
 
--- All TA assignment pairs that can occur and actually occurred
+-- All TA &assignment pairs that can occur and actually occurred
 CREATE VIEW all_tas_pairs AS(
 	SELECT DISTINCT username, assignment_id
 	FROM all_pairs
@@ -65,6 +64,7 @@ CREATE VIEW exist_tas_pairs AS(
 	FROM exist_pairs
 );
 
+-- All TAs that are disqualified based on criterion
 CREATE VIEW not_output_ta AS (
 	SELECT username AS ta
 	FROM ((SELECT DISTINCT username 
